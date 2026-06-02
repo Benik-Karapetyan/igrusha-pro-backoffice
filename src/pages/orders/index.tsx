35efeba83368c400a1712adbe26c0dba3ca8b6cf
Benik <benik.karapetyan1@gmail.com>
2026-06-02 19:55:58 +0400

    fix: 🐛 minor

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -100,15 +100,6 @@ export const OrdersPage = () => {
     }
   };
 
-  const handleMigrateOrders = async () => {
-    try {
-      await api.post("/orders/backfill-item-prices");
-      toast.success("Orders migrated successfully");
-    } catch (err) {
-      toast.error(getErrorMessage(err));
-    }
-  };
-
   const getOrders = useCallback(async () => {
     try {
       setLoading(true);
@@ -149,11 +140,7 @@ export const OrdersPage = () => {
 
   return (
     <div>
-      <AppHeader
-        title="Orders"
-        SecondaryButton={<Button onClick={handleMigrateOrders}>Migrate Orders</Button>}
-        MainButton={<Button onClick={handleAddClick}>Add Order</Button>}
-      />
+      <AppHeader title="Orders" MainButton={<Button onClick={handleAddClick}>Add Order</Button>} />
 
       <div className="flex gap-4 p-4 pb-0">
         <div className="w-[250px]">
